bd690321111f7864b60923d397c8747661f02d4e
Dimitar <bulgaria_mitko@yahoo.com>
2015-09-25 15:20:34 +0300

    just a demo commit...
    this is just a demo commit to see if everything is working...

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
+namespace _02ReplaceTag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("just a demo commit");
+        }
+    }
+}
