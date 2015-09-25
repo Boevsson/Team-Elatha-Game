c7754a1ffc5518f08ba99ee08e1bf3acfc25ecfd
Dimitar <bulgaria_mitko@yahoo.com>
2015-09-25 15:30:16 +0300

    fixing the first commit i did which made errors
    it should be fine now.

--- Program.cs
+++ Program.cs
@@ -1,24 +1,9 @@
-<<<<<<< HEAD
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-
-namespace _02ReplaceTag
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-
-            Console.WriteLine("just a demo commit");
-        }
-    }
-}
-=======
-using System.Text;
 
 class Game
 {
@@ -29,4 +14,3 @@ class Game
         System.Console.WriteLine(str);
     }
 }
->>>>>>> origin/master
