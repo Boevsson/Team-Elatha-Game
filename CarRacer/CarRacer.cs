5b5c222c9dfe1a66c53a0946b46656f3875a7670
immperius <pgboev@abv.bg>
2015-09-29 07:34:25 +0300

    Update CarRacer.cs

--- CarRacer/CarRacer.cs
+++ CarRacer/CarRacer.cs
@@ -4,6 +4,8 @@
     {
         static void Main()
         {
+            Console.BufferHeight = Console.WindowHeight = 45; 
+            Console.BufferWidth = Console.WindowWidth = 70; 
             Game game1 = new Game();
             game1.PlayGame();
         }
