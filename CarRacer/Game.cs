48b25476bb704b74eec7ced947e25c695859bd48
Ivan Yanchev <ivan.yan4ev@gmail.com>
2015-09-30 20:05:14 +0300

    Minor bug fix

--- CarRacer/Game.cs
+++ CarRacer/Game.cs
@@ -15,7 +15,7 @@ namespace CarRacer
         protected static int trackOffsetRight = 25;
         protected string highScoreFilePath = @"Scores.txt";
         protected double score = 0;
-        protected int lives = 1;
+        protected int lives = 3;
         protected string player = string.Empty;
 
         public void PlayGame()
@@ -142,7 +142,8 @@ namespace CarRacer
             List<Car> carsList = new List<Car>();
             List<Coin> collectibles = new List<Coin>();
 
-            // initialize player car
+        // initialize player car
+        RestartRace:
             Car myCar = new Car(34, 35, ConsoleColor.Red);
 
             Random random = new Random();
@@ -158,7 +159,7 @@ namespace CarRacer
                 {
                     Coin bonus = new Coin();
                     int bonusLane = random.Next(0, 5);
-                    bonus.X = trackOffsetRight + 2 + 4 * bonusLane; 
+                    bonus.X = trackOffsetRight + 2 + 4 * bonusLane;
                     // 21-> where the first lane starts; 2-> half the width of the lane; 4-> the width of one lane
                     bonus.Y = 1;
                     collectibles.Add(bonus);
@@ -237,12 +238,13 @@ namespace CarRacer
                     {
                         PrintCarAtPosition(myCar.X, myCar.Y, "X", ConsoleColor.Yellow);
                         score += 10;
+                        bonusCoin.Y = Console.WindowHeight;
                     }
 
                 }
                 for (int i = 0; i < collectibles.Count; i++)
                 {
-                    if (collectibles[i].Y > Console.WindowHeight - 1)
+                    if (collectibles[i].Y >= Console.WindowHeight - 1)
                     {
                         collectibles.Remove(collectibles[i]);
                     }
@@ -298,7 +300,7 @@ namespace CarRacer
                     //}
                 }
                 score += (0.2) * speed / 240;
-                Thread.Sleep(250-speed);
+                Thread.Sleep(250 - speed);
                 Console.Clear();
                 newCarInterval++;
                 newCollectibleInterval++;
@@ -632,7 +634,7 @@ namespace CarRacer
                     Console.Clear();
                 }
             }
-    }
+        }
 
         private void centerText(String text)
         {
@@ -644,9 +646,9 @@ namespace CarRacer
         private void PrintLogo(int x, int y, string logo)
         {
             Console.SetCursorPosition(x, y);
-            
 
-            
+
+
 
         } // end private void PrintLogo(int x, int y)
 
@@ -736,6 +738,6 @@ namespace CarRacer
         } // end private void PrintAtPosition(int x, int y, char symbol, ConsoleColor color)
 
         #endregion
-        
+
     } // end class Game
 } // end namespace CarRacer
